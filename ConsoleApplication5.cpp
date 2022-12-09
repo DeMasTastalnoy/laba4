66336823f579caa339446b4459e36e1582a90b7e
DeMasTastalnoy <115668334+DeMasTastalnoy@users.noreply.github.com>
2022-12-10 01:16:57 +0300

    Add files via upload

---
+++ ConsoleApplication5.cpp
@@ -0,0 +1,34 @@
+﻿
+#include <iostream>
+#include <locale>
+
+using namespace std;
+
+void remove(char* exp, int* startindex, int* count)
+{
+    for (int i = *startindex; i < *count + *startindex; i++)
+    {
+        exp[i] = ' ';
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int startindex;
+    int count;
+    const unsigned short size = 100;
+    char* exp = new char[size];
+    cin >> exp >> startindex >> count;
+    while (startindex > size || count > (size - startindex))
+    {
+        startindex = 0;
+        count = 0;
+        cin >> startindex >> count;
+    }
+    remove(exp, &startindex, &count);
+    cout << endl;
+    cout << exp;
+    delete[] exp;
+    return 0;
+}
